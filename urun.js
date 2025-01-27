8c48e5fe67fdc4758d87b4494cc72ace486effe7
Kitis <94494409+Kitis2cool@users.noreply.github.com>
2025-01-27 10:41:36 -0800

    Update urun.js

--- urun.js
+++ urun.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=800,height=500");
 		var e = t.document.createElement("iframe");
-		(e.src = "//raw.githubusercontent.com/uStuff/uRun/refs/heads/main/popup.html"),
+		(e.src = "raw.githubusercontent.com/uStuff/uRun/refs/heads/main/popup.html"),
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
 			t.document.title = "KitisP uRun",
