bf931936a0a77ba31efe5bc8684266213d1baf16
Kitis <94494409+Kitis2cool@users.noreply.github.com>
2025-01-27 14:35:20 -0800

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,22 +1,14 @@
 /// urun.js
 document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
-		var t = window.open("about:blank", "_blank", "width=800,height=500");
-        
-        // Ensure the new window has a blank document
-        var iframe = t.document.createElement("iframe");
-        iframe.src = "https://raw.githubusercontent.com/uStuff/uRun/refs/heads/main/popup.html";
-        iframe.style.cssText = "width:100%; height:100%; border:none;";
-        
-        // Append the iframe to the new window's body
-        t.document.body.appendChild(iframe);
-        t.document.title = "KitisP uRun Discord: .Kitis";
-        
-        // Listen for messages from the iframe
-        t.addEventListener("message", function (e) {
-            if (e.data.toString().startsWith("execute:")) {
-                eval(e.data.toString().replace("execute:", ""));
-                t.close();
+		var t = window.open("", "_blank", "width=800,height=500");
+		var e = t.document.createElement("iframe");
+		(e.src = "https://raw.githubusercontent.com/uStuff/uRun/refs/heads/main/popup.html"),
+			(e.style.cssText = "width:100%; height:100%; border:none;"),
+			t.document.body.appendChild(e),
+			t.document.title = "KitisP uRun Discord: .Kitis",
+			t.addEventListener("message", function (e) {
+				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
 			});
 	}
 });
